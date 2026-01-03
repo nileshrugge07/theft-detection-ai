be3b6eafdc9b07f8b3e6cfa8ee5328fae77ca49e
nileshrugge07 <106425351+nileshrugge07@users.noreply.github.com>
2026-01-03 21:30:56 +0530

    app.py

--- app.py
+++ app.py
@@ -156,4 +156,4 @@ def stop_camera():
 # START
 # ======================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True, threaded=True)
+    app.run(host="0.0.0.0",debug=True, threaded=True,port=port)
